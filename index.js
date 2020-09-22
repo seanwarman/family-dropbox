8535f19970ba29aac17915568ee16bfa27776f90
Sean Warman <seanwarman@Seans-MacBook-Pro.local>
2020-09-22 07:19:58 +0100

    set up environment

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { DB_PASSWORD, DB_HOST, DB_PORT, DB_USER, DB_NAME } from '@env';
+
+console.log('DB_PASSWORD: ', DB_PASSWORD)
 
 AppRegistry.registerComponent(appName, () => App);
