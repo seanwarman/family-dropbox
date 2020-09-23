95c2913371ad99b93e4bc4bf1b7606104d4135e3
Sean Warman <seanwarman@Seans-MacBook-Pro.local>
2020-09-23 04:55:51 +0100

    example tab page

--- App.js
+++ App.js
@@ -4,18 +4,24 @@ import { StyleSheet } from 'react-native';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Layout } from '@ui-kitten/components';
 import { default as theme } from './ui-kitten-custom-theme.json';
-import { store } from './store'
+import { store } from './store';
+import Pager from './components/Pager';
 
 const App = () => (
   <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
     <Provider store={store}>
-      <Layout style={styles.layout}>
+      <Layout style={styles.container}>
+        <Pager />
       </Layout>
     </Provider>
   </ApplicationProvider>
 );
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
   layout: {
     flex: 1,
     justifyContent: 'center',
